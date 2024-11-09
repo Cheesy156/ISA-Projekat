5315027d3ddf47289660d9db10c23336061971c8
Cheesy156 <danilo00kg@gmail.com>
2024-11-09 17:56:03 +0100

    Adding atomic

--- isaprojekat/isaprojekat/view.py
+++ isaprojekat/isaprojekat/view.py
@@ -5,14 +5,23 @@ from .serializers import UserRegistrationSerializer, UserLoginSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
 
+import time
+from django.db import transaction, IntegrityError
+
 @api_view(['POST'])
 def register_user(request):
-    serializer = UserRegistrationSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    time.sleep(2)
+
+    with transaction.atomic():
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                serializer.save()
+                return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
+            except IntegrityError:
+                return Response({"error": "A user with this username or email already exists."}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def login_user(request):
@@ -22,8 +31,8 @@ def login_user(request):
         # Generate JWT tokens for the user
         refresh = RefreshToken.for_user(user)
         return Response({
-            "refresh": str(refresh),
-            "access": str(refresh.access_token),
+            "refresh": str(refresh), # Probably wont be used for this project
+            "access": str(refresh.access_token), 
             "message": "Login successful!"
         }, status=status.HTTP_200_OK)
     
